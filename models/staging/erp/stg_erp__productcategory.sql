59e07f26a9f1bdd48d4e3095e4fb0e621a4014da
Nicole Corrêa <nicoleoceano@gmail.com>
2025-01-23 20:17:35 +0000

    criada a configuração global

---
+++ models/staging/erp/stg_erp__productcategory.sql
@@ -0,0 +1,11 @@
+with 
+    src_productcategory as (
+    select
+        cast( PRODUCTCATEGORYID as int) as id_productcategory
+        , cast(NAME as string) as name_productcategory
+        
+    from {{ source('erp_adventure_works', 'productcategory') }}
+)
+
+select *
+from src_productcategory
