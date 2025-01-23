61a4755f31a5bfb203674c86c0fbe5cad9f939fc
Nicole Corrêa <nicoleoceano@gmail.com>
2025-01-23 20:58:45 +0000

    criadas as stagings de produto

--- models/staging/erp/stg_erp__productcategory.sql
+++ models/staging/erp/stg_erp__productcategory.sql
@@ -1,7 +1,7 @@
 with 
     src_productcategory as (
     select
-        cast( PRODUCTCATEGORYID as int) as id_productcategory
+        cast( PRODUCTCATEGORYID as int) as pk_productcategory
         , cast(NAME as string) as name_productcategory
         
     from {{ source('erp_adventure_works', 'productcategory') }}
---
+++ models/staging/erp/stg_erp__productsubcategory.sql
@@ -0,0 +1,12 @@
+with 
+    src_productsubcategory as (
+    select
+        cast( PRODUCTSUBCATEGORYID as int) as pk_productsubcategory
+        , cast(PRODUCTCATEGORYID as int) as fk_productcategory
+        , cast(NAME as string) as name_productcategory
+        
+    from {{ source('erp_adventure_works', 'productsubcategory') }}
+)
+
+select *
+from src_productsubcategory
