168d44ed3159518bc27b3027973db5e7422e580f
Nicole Corrêa <nicoleoceano@gmail.com>
2025-01-24 13:49:17 +0000

    criada a staging para sales reason

---
+++ models/staging/erp/stg_erp__salesreason.sql
@@ -0,0 +1,12 @@
+with 
+    src_salesreason as (
+    select
+         cast(SALESREASONID as int) as pk_salesreason
+            , cast(NAME as string) as name_salesreason
+            , cast(REASONTYPE as string) as type_salesreason
+        
+    from {{ source('erp_adventure_works', 'salesreason') }}
+)
+
+select *
+from src_salesreason
