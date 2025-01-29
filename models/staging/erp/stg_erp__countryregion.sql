248e1780ecdf6c9d544e91528f84ab2d6e090e80
Nicole Corrêa <nicoleoceano@gmail.com>
2025-01-29 14:21:35 +0000

    criadas as stg de salesorderdetail e salesorderheader

--- models/staging/erp/stg_erp__countryregion.sql
+++ models/staging/erp/stg_erp__countryregion.sql
@@ -2,7 +2,7 @@ with
     src_countryregion as (
     select
          cast(COUNTRYREGIONCODE as string) as pk_countryregion
-            , cast(NAME as string) as name_country
+        , cast(NAME as string) as name_country
         
     from {{ source('erp_adventure_works', 'countryregion') }}
 )
